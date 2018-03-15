a71140d2b946532f11c1bc8aa4e2d0bacbf777a5
Don Park <donpark@docuverse.com>
2018-03-15 10:34:30 -0700

    use space as shift

--- keyboards/planck/keymaps/donpark/keymap.c
+++ keyboards/planck/keymaps/donpark/keymap.c
@@ -40,6 +40,7 @@ enum planck_keycodes {
 
 #define ESC_POK3R  LT(_POK3R, KC_ESC)
 #define ENT_SHIFT  MT(MOD_RSFT, KC_ENT)
+#define SPC_LSFT   MT(MOD_LSFT, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -55,10 +56,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,      KC_BSPC},
-  {ESC_POK3R, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,   KC_QUOT},
-  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,   ENT_SHIFT},
-  {BACKLIT,   KC_LALT, KC_LGUI, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT,  KC_RGHT,  KC_UP,     KC_DOWN}
+  {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,      KC_BSPC},
+  {ESC_POK3R, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,   KC_QUOT},
+  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,   ENT_SHIFT},
+  {BACKLIT,   KC_LALT, KC_LGUI, KC_LCTL, LOWER,   SPC_LSFT, SPC_LSFT,  RAISE,   KC_LEFT,  KC_RGHT,  KC_UP,     KC_DOWN}
 },
 
 /* Pok3r
