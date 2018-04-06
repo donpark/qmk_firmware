1de21a098d87fda5580eb4d23865989952c098d2
Don Park <donpark@docuverse.com>
2018-04-06 00:01:38 -0700

    undo swap

--- keyboards/planck/keymaps/donpark/keymap.c
+++ keyboards/planck/keymaps/donpark/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,      KC_BSPC},
   {POK3R,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,   KC_QUOT},
   {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,   ENT_SHIFT},
-  {KC_LCTL,   KC_LALT, KC_LGUI, KC_ESC,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT,  KC_RGHT,  KC_UP,     KC_DOWN}
+  {KC_ESC,    KC_LALT, KC_LGUI, KC_LCTL,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT,  KC_RGHT,  KC_UP,     KC_DOWN}
 },
 
 /* Pok3r
