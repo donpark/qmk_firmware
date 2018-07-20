0c4fa77df7ddfd3f97bdeca483aec1f404443759
Don Park <donpark@docuverse.com>
2018-07-20 08:17:52 -0700

    tap dance fix

--- keyboards/vitamins_included/keymaps/donpark/config.h
+++ keyboards/vitamins_included/keymaps/donpark/config.h
@@ -41,4 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                               }
 #endif
 
+#undef NO_ACTION_TAPPING
+#undef RGBLIGHT_ANIMATIONS
+
 #endif
--- keyboards/vitamins_included/keymaps/donpark/rules.mk
+++ keyboards/vitamins_included/keymaps/donpark/rules.mk
@@ -1,3 +1,5 @@
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
 endif
