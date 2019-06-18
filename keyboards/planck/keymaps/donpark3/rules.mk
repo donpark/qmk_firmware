62b01a207bcbe643107e2fa785ee6b86658b2f96
Don Park <donpark@docuverse.com>
2019-06-18 01:27:49 -0700

    use LINK_TIME_OPTIMIZATION_ENABLE

--- keyboards/planck/keymaps/donpark3/rules.mk
+++ keyboards/planck/keymaps/donpark3/rules.mk
@@ -2,4 +2,6 @@ SRC += muse.c
 
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
-AUDIO_ENABLE = no           # Audio output on port C6
+AUDIO_ENABLE = yes          # Audio output on port C6
+
+LINK_TIME_OPTIMIZATION_ENABLE = yes
