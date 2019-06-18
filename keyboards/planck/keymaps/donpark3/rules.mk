0d07f9a8a40d359b231b83ddfc8ad5acc00edf0f
Don Park <donpark@docuverse.com>
2019-06-17 21:58:21 -0700

    reduce firmware size

--- keyboards/planck/keymaps/donpark3/rules.mk
+++ keyboards/planck/keymaps/donpark3/rules.mk
@@ -1,1 +1,5 @@
 SRC += muse.c
+
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+CONSOLE_ENABLE = no         # Console for debug(+400)
+AUDIO_ENABLE = no           # Audio output on port C6
