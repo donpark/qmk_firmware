2eb6a2524d7c3aaeb4edcf5f9c040c0812f3ee9a
Don Park <donpark@docuverse.com>
2019-07-17 01:48:32 -0700

    final change

--- keyboards/lily58/keymaps/donpark/keymap.c
+++ keyboards/lily58/keymaps/donpark/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC,  KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTER,   \
-                             KC_LALT, KC_LGUI, LOWER, KC_SPC,   KC_SPC,  RAISE,   KC_PGUP, KC_PGDN \
+                             KC_LALT, KC_LGUI, LOWER, KC_BSPC,  KC_SPC,  RAISE,   KC_PGUP, KC_PGDN \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, _______, KC_UP,   _______, _______, _______, \
   _______, _______, _______, _______, KC_F11,  KC_F12,                    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                             _______, _______, _______, KC_BSPC, KC_BSPC, _______, KC_HOME, KC_END\
+                             _______, _______, _______, _______, KC_ENT,  _______, KC_HOME, KC_END\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, _______, KC_UP,   _______, _______, _______, \
   _______, _______, _______, _______, KC_F11,  KC_F12,                    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                             _______, _______, _______, KC_BSPC, KC_BSPC, _______, KC_HOME, KC_END\
+                             _______, _______, _______, _______, KC_ENT,  _______, KC_HOME, KC_END\
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
