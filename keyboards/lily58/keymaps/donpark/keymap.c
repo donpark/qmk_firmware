4955fdee523bc9ec3f16b68b0381e3bd87da3616
Don Park <donpark@docuverse.com>
2019-07-18 00:24:11 -0700

    full board

--- keyboards/lily58/keymaps/donpark/keymap.c
+++ keyboards/lily58/keymaps/donpark/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC,  KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTER,   \
-                             KC_LALT, KC_LGUI, LOWER, KC_BSPC,  KC_SPC,  RAISE,   KC_PGUP, KC_PGDN \
+                             KC_LALT, KC_LGUI, LOWER, KC_BSPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, _______, _______, _______, _______, _______, \
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, _______, KC_UP,   _______, _______, _______, \
   _______, _______, _______, _______, KC_F11,  KC_F12,                    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                             _______, _______, _______, _______, KC_ENT,  _______, KC_HOME, KC_END\
+                             _______, _______, _______, _______, KC_ENT,  _______, _______, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -93,11 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  _______, \
-  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, _______, KC_UP,   _______, _______, _______, \
-  _______, _______, _______, _______, KC_F11,  KC_F12,                    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, _______, _______, _______, _______, _______, \
+  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, _______, KC_PGUP, _______, _______, _______, \
+  _______, _______, _______, _______, KC_F11,  KC_F12,                    _______, KC_HOME, KC_PGDN, KC_END,  KC_DEL,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                             _______, _______, _______, _______, KC_ENT,  _______, KC_HOME, KC_END\
+                             _______, _______, _______, _______, KC_ENT,  _______, _______, _______\
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
